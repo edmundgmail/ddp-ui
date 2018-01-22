10408071a0a8548b439ff87c646eeede597eafce
eguo <eguo@localhost.localdomain>
2018-01-22 06:34:30 -0500

    update

--- src/app/models/tab.ts
+++ src/app/models/tab.ts
@@ -1,4 +1,4 @@
 export class Tab {
-  title:string;
+  name: string;
   content: string;
 }
--- src/app/sql-snippets/sql-snippets.component.ts
+++ src/app/sql-snippets/sql-snippets.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sql-snippets',
   templateUrl: './sql-snippets.component.html',
-  styleUrls: ['./sql-snippets.component.css']
+  styleUrls: ['../app.component.css']
 })
 export class SqlSnippetsComponent implements OnInit {
+  qtd:any[] = {};
 
   constructor() { }
 
